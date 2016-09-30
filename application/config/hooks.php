ecaee8f5d94b1b7b14b3ea42f9e740447213a0c3
Daniel Lin <danlin604@gmail.com>
2016-09-30 13:51:59 -0700

    Hook bleeps the bleeps
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+$hook['display_override'] = array
+(
+	'class'    => 'CensorHook',
+	'function' => 'censor',
+	'filename' => 'CensorHook.php',
+	'filepath' => 'hooks',
+	'params'   => array('controller')
+);
