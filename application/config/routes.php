374ba1e043beceb4833f83ec2043852a1f64338e
Norman Lim <Norman.Lim2@hotmail.com>
2016-09-30 12:22:27 -0700

    fixed lock/em/up controller with wildcard routing
    Signed-off-by: Norman Lim <Norman.Lim2@hotmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,8 +50,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['sleep'] = 'first/zzz';
+$route['lock/(:any)/(:any)'] = 'welcome/shucks';
+
 
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
 
